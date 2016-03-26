694fce508ccd2d589ca64ddf8fc68c0b2c75fa52
Caner Uguz <caneruguz@gmail.com>
2016-03-26 11:45:38 -0400

    Finalize with filtering

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-$('#addContactButton').click(function(){
-	var name = $('#nameField').val();
-	$('.userContainer').prepend('<div class="contact-list-item">' + name + '</div>');
+$('#addContactButton').on('click', function(){
+	var element = $('#nameField');
+	var name = element.val();
+	$('.userContainer').prepend('<div class="contact-list-item"><span>' + name + '</span> <button class="btn btn-sm btn-danger remove-contact">Delete </button></div>');
+	element.val('').focus();
 });
 
 
+$('body').on('click', '.remove-contact', function(){
+	console.log(this, event );
+	var parent = $(this).parent();
+	parent.remove();
+});
 
+$('#showMe').click(function(){
+	var people = $('.contact-list-item');
+	console.log(people.length);
+});
+
+$('#searchItems').keyup(function(){
+	var val = $(this).val().toLowerCase();
+	$('.contact-list-item').each(function(){
+		var el = $(this);
+		var content = el.find('span').text().trim().toLowerCase();
+		console.log(content);
+		var exists = content.indexOf(val) !== -1;
+		if(exists){
+			el.show();
+		} else {
+			el.hide();
+		}
+	});
+
+});
 
 
 
