e9abd1e3a3ed85706c4da546b13691802dfcf06e
Caner Uguz <caneruguz@gmail.com>
2016-03-26 10:51:56 -0400

    Add individual items as response to add button click

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var contacts = [];
+$('#addContactButton').click(function(){
+	var name = $('#nameField').val();
+	$('.userContainer').prepend('<div class="contact-list-item">' + name + '</div>');
+});
+
+
+
+
+
+
+
 
